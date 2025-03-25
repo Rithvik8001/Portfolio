cdfc8f8956f30833809699ddc49a8635c32dfe4c
Rithvik Pallamreddy <152109655+Rithvik8001@users.noreply.github.com>
2025-03-25 09:26:12 -0400

    Update page.tsx

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -13,7 +13,7 @@ const AboutPage = () => {
       <div className="mt-4 flex flex-col gap-4">
         <p>
           Hey, my name is Rithvik and welcome to my space on the internet. I'm a
-          self-taught React Native / Web developer based in Louisville,KY.
+          self-taught IOS / Web developer based in Louisville,KY.
         </p>
         <p>
           Learning to code has felt like a superpower for me, it allows me to
