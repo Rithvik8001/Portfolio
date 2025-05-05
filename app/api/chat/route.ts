d77410b871ac402eb1badd658de67e8ee67a0904
Rithvik Pallamreddy <1017rithvik@gmail.com>
2025-05-05 07:58:23 -0400

    colophon page

---
+++ app/api/chat/route.ts
@@ -0,0 +1,22 @@
+import { aboutMe } from "@/lib/utils";
+import { createOpenAI } from "@ai-sdk/openai";
+import { streamText, convertToCoreMessages } from "ai";
+
+export const maxDuration = 30;
+
+const openAI = createOpenAI({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+
+export async function POST(req: Request) {
+  const { messages } = await req.json();
+
+  const result = streamText({
+    model: openAI("gpt-4o-mini"),
+    temperature: 0.5,
+    messages: convertToCoreMessages(messages),
+    system: aboutMe(),
+  });
+
+  return result.toDataStreamResponse();
+}
