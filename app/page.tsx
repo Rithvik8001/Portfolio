8802befe608a7be235f5ffbe29fcfafec21531ed
Rithvik <1017rithvik@gmail.com>
2025-11-29 10:30:33 -0500

    home page

--- app/page.tsx
+++ app/page.tsx
@@ -1,74 +1,51 @@
-import Link from "next/link";
-import { FileUser, Github, Lightbulb, Linkedin, Mail } from "lucide-react";
-import ProjectsSection from "@/components/home-page/projects-section";
-import Image from "next/image";
-import signature from "@/assets/signature.png";
-import Footer from "@/components/shared/footer";
-import Tags from "@/components/home-page/tags";
+import { GitHubContributions } from "@/components/home-page/github-contributions";
+import { AboutSection } from "@/components/home-page/about-section";
+import { Overview } from "@/components/home-page/overview/overview";
+import { ProfileHeader } from "@/components/home-page/header-cover/profile-header";
+import { Skills } from "@/components/home-page/skills";
+import { SocialLinks } from "@/components/home-page/social-links/social-links";
+import SectionSeparator from "@/components/ui/section-separator";
+import { Experiences } from "@/components/home-page/experiences";
+import { ProfileCover } from "@/components/home-page/header-cover/profile-cover";
+import { Projects } from "@/components/home-page/projects/projects";
+import { Brand } from "@/components/home-page/brand";
 
 const HomePage = () => {
   return (
-    <main className="px-4">
-      <h1 className="font-serif text-[40px] leading-12 font-light text-primary">
-        Rithvik Pallamreddy
-      </h1>
-      <Tags />
-      <p className="mt-4 text-muted-foreground">
-        A self taught Full Stack Web Developer following his dream of delivering
-        smiles with delightful experiences ☻
-      </p>
-      <section className="grid w-fit grid-cols-3 gap-x-6 gap-y-2 pt-4 tracking-tight md:flex md:flex-row md:items-start">
-        <Link
-          target="_blank"
-          href="https://drive.google.com/file/d/1bOheLtCI-yqD0TcP-DnGY1_YZqd1xQ_h/view?usp=sharing"
-          className="flex items-center gap-1.5 underline-offset-2 transition-colors hover:text-primary hover:underline"
-        >
-          <FileUser className="size-[14px]" /> <p>resume</p>
-        </Link>
-        <Link
-          target="_blank"
-          href="https://www.linkedin.com/in/rithvik-pallamreddy/"
-          className="flex items-center gap-1.5 underline-offset-2 transition-colors hover:text-primary hover:underline"
-        >
-          <Linkedin className="size-[14px]" /> <p>linkedin</p>
-        </Link>
-        <Link
-          target="_blank"
-          href="https://github.com/Rithvik8001"
-          className="flex items-center gap-1.5 underline-offset-2 transition-colors hover:text-primary hover:underline"
-        >
-          <Github className="size-[14px]" /> <p>github</p>
-        </Link>
-        <Link
-          target="_blank"
-          href="mailto:1017rithvik@gmail.com"
-          className="flex items-center gap-1.5 underline-offset-2 transition-colors hover:text-primary hover:underline"
-        >
-          <Mail className="size-[14px]" /> <p>email</p>
-        </Link>
-      </section>
-      <ProjectsSection />
-      <div className="mt-4 w-fit rounded-md border border-muted-foreground/20 bg-primary-foreground px-2 py-1">
-        <p className="[font-size:_12px_!important]">
-          <span>
-            <Lightbulb className="mr-1 mb-1 inline-block size-[12px]" />
-          </span>
-          Feel free to explore my{" "}
-          <Link
-            className="[font-size:_12px_!important] text-muted underline underline-offset-2"
-            href="https://github.com/Rithvik8001"
-            target="_blank"
-          >
-            GitHub
-          </Link>{" "}
-          for more projects.
-        </p>
-      </div>
-      <div className="flex w-full items-center justify-center pt-8">
-        <Image src={signature} alt="Signature" className="w-64" />
-      </div>
-      <Footer />
-    </main>
+    <div className="mx-auto md:max-w-3xl *:[[id]]:scroll-mt-22">
+      <ProfileCover />
+      <ProfileHeader />
+      <SectionSeparator />
+
+      <Overview />
+      <SectionSeparator />
+
+      <SocialLinks />
+      <SectionSeparator />
+
+      <AboutSection />
+      <SectionSeparator />
+
+      <Skills />
+      <SectionSeparator />
+
+      <GitHubContributions />
+      <SectionSeparator />
+
+      <Experiences />
+      <SectionSeparator />
+
+      <Projects />
+      <SectionSeparator />
+
+      {/*
+        <Certifications />
+        <Separator /> 
+        */}
+
+      <Brand />
+      <SectionSeparator />
+    </div>
   );
 };
 
